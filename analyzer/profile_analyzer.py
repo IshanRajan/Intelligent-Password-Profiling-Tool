701f60d570a3639180940c03774d48a4c50bcdbc
IshanRajan28 <irajan@terpmail.umd.edu>
2025-10-27 14:07:08 -0400

    Worked on profile_analyzer.py, I moved the has_uppercase_loop, contains_number_loop, contains_special_chars_regex, outside the loop to make it more effective and I added a new password category for very strong passwords. I plan to make the code more efficient.

--- analyzer/profile_analyzer.py
+++ analyzer/profile_analyzer.py
@@ -1,3 +1,62 @@
+import re
+
+def has_uppercase_loop(password):
+    length_password = len(password)
+    count = 0
+    for char in password:
+        if char.isupper():
+            count += 1
+                
+        else:
+            pass
+            
+    if length_password == count:
+        return "all letters are capitalize"
+            
+    elif count == 0:    
+        return False
+            
+    else:
+        return True
+
+def contains_number_loop(password):
+    length_password = len(password)
+    count = 0
+    for char in password:
+        if char.isdigit():
+            count += 1
+                
+        else:
+            pass
+            
+    if length_password == count:
+        return "password contains only numbers"
+            
+    elif count == 0:
+        return False
+            
+    else:
+        return True
+
+def contains_special_chars_regex(password):
+    length_password = len(password)
+    count = 0
+    for char in password:
+        # Matches any character that is NOT a letter (a-z, A-Z), digit (0-9), or space
+        if re.search(r'[^a-zA-Z0-9]', char):
+            count += 1
+                
+        else:
+            pass
+            
+    if length_password == count:
+        return "password contains only special characters"
+            
+    elif count == 0:
+        return False
+            
+    else:
+        return True
 
 def profile_analyzer(usernames,usernames_variations, generated_passwords, profile):
     
@@ -12,6 +71,7 @@ def profile_analyzer(usernames,usernames_variations, generated_passwords, profil
     weak_passwords = []
     medium_passwords = []
     strong_passwords = []
+    very_strong_passwords = []
     
     for password in generated_passwords:
         password_score = 0
@@ -46,29 +106,10 @@ def profile_analyzer(usernames,usernames_variations, generated_passwords, profil
             password_score += 2
         
         elif 8<=len(password)<=12:
-            pasword_score += 1
+            password_score += 1
         
         else:
             password_score += 0
-        
-        def has_uppercase_loop(password):
-            length_password = len(password)
-            count = 0
-            for char in password:
-                if char.isupper():
-                    count += 1
-                
-                else:
-                    pass
-            
-            if length_password == count:
-                return "all letters are capitalize"
-            
-            elif count == 0:    
-                return False
-            
-            else:
-                return True
             
         if has_uppercase_loop(password) == True:
             password_score += 2
@@ -77,34 +118,61 @@ def profile_analyzer(usernames,usernames_variations, generated_passwords, profil
             password_score += 1
         
         else:
-            password += 0
-        
-        def contains_number_loop(password):
-            length_password = len(password)
-            count = 0
-            for char in password:
-                if char.isdigit():
-                    count += 1
-                
-                else:
-                    pass
-            
-            if length_password == count:
-                return "password contains only numbers"
-            
-            elif count == 0:
-                return False
-            
-            else:
-                return True
+            password_score += 0
             
         if contains_number_loop(password) == True:
             password_score += 2
         
         elif contains_number_loop(password) == "password contains only numbers":
-            password_score += 1
+            password_score += 0
         
         else:
+            password_score += 0
+        
+        if contains_special_chars_regex(password) == True:
+            password_score += 2
+        
+        elif contains_special_chars_regex(password) == False:
+            password_score += 0
+        
+        else:
+            password_score += 0
+        
+        if "aaaaa" in password:
+            password_score -= 1
+        
+        if "12345" in password:
+            password_score -= 1
+        
+        if "abcd" in password:
+            password_score -= 1
+        
+        if password_score <= 2:
+            weak_passwords.append(password)
+        
+        elif 3 <= password_score <= 5:
+            medium_passwords.append(password)
+        
+        elif 7 <= password_score <= 10:
+            strong_passwords.append(password)
+        
+        else:
+            very_strong_passwords.append(password)
+        
+        weak_passwords = sorted(list(set(weak_passwords)))
+        medium_passwords = sorted(list(set(medium_passwords)))
+        strong_passwords = sorted(list(set(strong_passwords)))
+        very_strong_passwords = sorted(list(set(very_strong_passwords)))
+    
+    return weak_passwords, medium_passwords, strong_passwords, very_strong_passwords
+        
+
+        
+            
+            
+            
+        
+        
             
         
         

8d9d08bc4d11774788476e9c011ad681ca4d307b
IshanRajan28 <irajan@terpmail.umd.edu>
2026-01-26 15:29:27 -0500

    Worked on the main.py, added a new input on the command line and planning to work on the report_generator.py.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from input.cli_input import get_user_info
 from data.data_loader import load_csv, load_json, load_text
 from generator.username_generator import generate_username, expand_username_variations
+from generator.password_generator import generate_passwords
 from pathlib import Path
 
 
@@ -17,6 +18,12 @@ def parse_arguments():
         "-i", "--input", type=str,
         default=None,
         help="Path to a profile input (if omitted, interactive mode is used)"
+    )
+
+    parser.add_argument(
+        "-o", "--output-format", type=str,
+        default="cli",
+        help="The file format you want the output to be stored in (Default being the command line)"
     )
 
     return parser.parse_args()
@@ -35,23 +42,49 @@ def main():
 
             elif extension == ".json":
                 user_information = load_json(args.input)
-            
+
             elif extension == ".txt":
                 user_information = load_text(args.input)
-            
+
             else:
                 print("Incorrect File Type, please input.csv, .json and .txt")
                 sys.exit(1)
         else:
-            # Maybe I can potentially loop here
-            user_information = [get_user_info()]
+            while True:
+                cont = print(
+                    "Do you wanna add the user information? \n [yes or no]")
+                cont_case = cont.lower()
+                if cont_case == "yes":
+                    user_information = [get_user_info()]
+
+                else:
+                    break
+
+        for profile in user_information:
+            usernames = generate_username(profile)
+            usernames_variations = expand_username_variations(
+                usernames, profile)
+            passwords = generate_passwords(profile)
+
+        if args.output_format == "cli":
+            pass
+            # Saves as a CLI
         
-        usernames = generate_username(user_information)
-        usernames_variations = expand_username_variations(usernames,user_information)
-    
+        elif args.output_format == "json":
+            pass
+            # Saves as a JSON
+        
+        elif args.output_format == "csv":
+            pass
+            # Saves as a CSV
+            
+        elif args.output_format == "txt":
+            pass
+            # Saves as a TXT
+
     except ValueError as e:
         print(f"Error: {e}")
-    
+
     except Exception as e:
         print(f"Unexpected error: {e}")
         sys.exit(1)

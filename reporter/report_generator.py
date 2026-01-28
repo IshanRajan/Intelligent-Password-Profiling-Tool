d9d72c102b6ce6b413729fa89f536d529288ec3f
Ishan Rajan <irajan@terpmail.umd.edu>
2026-01-28 16:10:13 -0500

    Worked on the report_generator.py and the format on how it would look like on the cli.

--- reporter/report_generator.py
+++ reporter/report_generator.py
@@ -6,10 +6,10 @@ def generate_report(profile_results, output_format):
     amount_of_profiles = len(profile_results)
     
     for p in profile_results:
-        total_usernames +=sum(len(p["usernames"]))
-        total_username_variations +=sum(len(p["username_variations"]))
+        total_usernames +=len(p["usernames"])
+        total_username_variations +=len(p["username_variations"])
+        total_passwords +=len(p["passwords"])
         
-    
     if output_format == "cli":
         print("========================================\n")
         print("Intelligent Password Profiling Tool\n")
@@ -18,8 +18,39 @@ def generate_report(profile_results, output_format):
         
         print("Summary\n")
         print("-------\n")
-        print(f"Profiles processed: {amount_of_profiles}")
+        print(f"Profiles processed: {amount_of_profiles}\n")
+        print(f"Total usernames generated: {total_usernames}\n")
+        print(f"Total username variations: {total_username_variations}\n")
         
+        for profile in profile_results:
+            print("----------------------------------------\n")
+            print(profile["profile"])
+            print(f"Name: {profile.get('first')} {profile.get('last')}\n")
+            print(f"Nickname: {profile.get('nickname')}\n")
+            print(f"Year: {profile.get('year')}\n")
+            print(f"Hobby: {profile.get('hobby')}\n")
+            print("\n----------------------------------------\n")
+            print("Fields used: first,last,nickname,year,hobby\n")
+            
+            print(f"Base Usernames {len(profile['usernames'])}\n")
+            print("--------------\n")
+            for usernames in profile["usernames"]:
+                print(f"{usernames}\n")
+            
+            print(f"Expand Username Variations {len(profile['username_variations'])}\n")
+            print("--------------\n")
+            for username_variation in profile["username_variations"]:
+                print(f"{username_variation}\n")
+            
+            print(f"Password Candidates {len(profile['passwords'])}\n")
+            print("--------------\n")
+            for password_variation in profile["passwords"]:
+                print(f"{password_variation}\n")
+            
+        print("========================================\n")
+        print("End of report\n")
+        print("========================================\n")
+               
     elif output_format == "json":
         pass
     
